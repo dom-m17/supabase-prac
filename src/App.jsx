454d517d1b87dcb23dfb4e416409e27360327733
habib <habibali15@hotmail.com>
2024-09-10 12:31:41 +0100

    installed auth ui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("https://<project>.supabase.co", "<your-anon-key>");
+const supabase = createClient("https://wayjxzeazxkexkdepytb.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndheWp4emVhenhrZXhrZGVweXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjUxMDksImV4cCI6MjA0MTU0MTEwOX0.Tuk5rgWsHMAhrgONpjeKLulX3uABUCbedFxjR-yW5Gk");
 
 function App() {
   const [countries, setCountries] = useState([]);
